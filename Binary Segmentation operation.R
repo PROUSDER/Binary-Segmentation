2b1498150b3075a84212cfda749258ac022a2c7a
Louis Forbes Wright <louis.forbes9@gmail.com>
2019-04-04 20:59:39 +0100

    updated the simulation study script and added the correct estimator for the variance

--- Binary Segmentation operation.R
+++ Binary Segmentation operation.R
@@ -23,6 +23,16 @@ Z_k <- function(data, k_1, k_2){
   return(test.stat)
 }  
 
+Bootstrap.of.error <- function(data, percentile = 99, simulation.number = 1000){
+  #we aim to find what effect we can expect the error to have on the CUSUM (Z_k) we are using, 
+  #assuming the error is standard normal. Then we take the 99%-ile of the simulated CUSUMs
+  #and use this to determine if a the maximum CUSUM score of a binary-segmentation operation is significant.
+  
+  
+  max.scores.attained <- NULL
+  size <- length(data)
+}
+
 construct.model <- function(sampl, ofs){
   
   est.signal <- NULL
@@ -31,77 +41,139 @@ construct.model <- function(sampl, ofs){
   for(i in 1:(length(ofs)-1)){
     a <- ofs[i]
     b <- ofs[i+1]
-    print(a)
-    print(b)
+
     est.signal <- c(est.signal, rep(mean(sampl[a:b]), (b-a)))
-    print(est.signal)
+    
     means <- c(means, mean(sampl[a:b]))
     }
   est.signal <- c(est.signal[1],est.signal)
   return(list(est.signal=est.signal, est.means=means))
 }
 
-Binary.segmentation.operation <- function(sample, passed_segments = NULL){
-  
+Binary.segmentation.operation <- function(sampl, passed_segments = NULL, boundary){
+
   changepoint_scores <- NULL
   estimated_changepoints <- NULL
   finalised_segments <- NULL
-  boundary <- length(sample)^(3/8)
+
   
   if(is.null(passed_segments)){ #the first operation on a dataset will require initalisation
-    working_segments <- c(1, length(sample))
-    finalised_segments <- c(1,length(sample))
+    working_segments <- c(1, length(sampl))
+    finalised_segments <- c(1,length(sampl))
   } else {
       working_segments <- sort(passed_segments)
       finalised_segments <- passed_segments
   }
   
-  
+
   
   #we need to order the estimated change-points so that the largest Z_k is first
-  for (i in 1:(length(working_segments)-1)){ # n points gives n-1 segments
-    if(max(Z_k(sample, working_segments[i],working_segments[i+1]), na.rm = TRUE)>= boundary){
-      #we take the maximum as a estimated change point
-     if(!((which.max(Z_k(sample, working_segments[i],working_segments[i+1]))+working_segments[i]) %in% passed_segments)){
-       #we test if the changepoinit is already accounted for 
-       estimated_changepoints <- c(estimated_changepoints, (which.max(Z_k(sample, working_segments[i],working_segments[i+1]))+working_segments[i]))
-       changepoint_scores <- c(changepoint_scores, max(Z_k(sample, working_segments[i],working_segments[i+1]), na.rm = TRUE))
+  for (i in 1:(length(working_segments)-1)){ # n changepoints gives n-1 segments
+    #we take the maximum as a estimated change point
+    # the zero removes the error message, but has no affect on the output of the function
+    if(max(c(Z_k(sampl, working_segments[i],working_segments[i+1]), 0), na.rm = TRUE)>= boundary){
+      ch <- which.max(Z_k(sampl, working_segments[i],working_segments[i+1]))+working_segments[i]
+      #we test if the changepoinit is already accounted for
+     if(!( ch %in% passed_segments)){
+        
+       estimated_changepoints <- c(estimated_changepoints, ch)
+       changepoint_scores <- c(changepoint_scores, max(Z_k(sampl, working_segments[i],working_segments[i+1]), na.rm = TRUE))
      }
     } 
   }
-  print(estimated_changepoints)
+ 
   if(!(is.null(estimated_changepoints))){
-    #print(estimated_changepoints)
-    #print(changepoint_scores)
+    
     bound <- cbind(estimated_changepoints, changepoint_scores)
-    #print(bound)
+    
     estimated_changepoints <- bound[order(-bound[,2]), 1]
-    #print(estimated_changepoints)
+   
   }
   finalised_segments <- c(finalised_segments, estimated_changepoints)
   return(unname(finalised_segments))
 }
 
 
-Binary.Segmentation <- function(sampl){
+Binary.Segmentation <- function(sampl, boundary = NULL, Bootstrap.settings = c(99 ,1000)){
+  #
+  # the function will apply binary segmentation to the supplied data.
+  # the function will use stored values for data with length less than 5000. 
+  #
+  # inputs   {sample data, significant value for the CUSUM}
+  #         @ sampl, boundary
+  #
+  # returns  {estimated signal, estimated means, estimated changepoints}
+  #         @ $est.signal, $est.means, $est.changepoints
+  #
+  #
+  
   held_segments <- (1)
   passed_segments <- NULL
   est.signal <- NULL
   
-  passed_segments <- Binary.segmentation.operation(sampl)
+      # as var(X_t - X_t-1) = 2var(X_t) 
+  sigma <- mad(diff(sampl)/sqrt(2)) 
+  
+  if(is.null(boundary)){
+    if(ceiling(length(sampl)/50)>length(gr)){
+      significant_value <- Bootstrap.of.error(sampl, Bootstrap.settings[1], Bootstrap.settings[2]) * sigma
+      
+      # the differencing in the median absolute deviation removes a linear trend if it exists from the data.
+      # Further improvements of the binary segmentation operation
+      # could see the addition of some methods to control for
+      # other trend structures in data (seasonality, sinusoidal models, etc)
+      
+      break
+      }else{significant_value <- gr[ceiling(length(sampl)/50)] * sigma}
+    }else{
+    significant_value <- boundary
+  }
+  #print(paste("using a significant value of",significant_value))
+  passed_segments <- Binary.segmentation.operation(sampl, boundary = significant_value)
   while(!setequal(held_segments, passed_segments)){
     # binary segmentation runs until the operation doesn't produce a new change-point
     held_segments <- passed_segments
-    passed_segments <- Binary.segmentation.operation(sampl, held_segments)
+    passed_segments <- Binary.segmentation.operation(sampl, held_segments, significant_value)
 
   }
-  print("in order of appearance the estimated changepoints are ")
+  #print("in order of occurence the estimated changepoints are ")
   ordered_finalised_segments <- sort(passed_segments)
-  #ordered_finalised_segments <- optimise.model(sampl,passed_segments)
-  print(ordered_finalised_segments)
+
+  #print(ordered_finalised_segments)
   output <- construct.model(sampl, ordered_finalised_segments)
   ofs <- list(est.changepoints = ordered_finalised_segments)
   output <- c(output, ofs)
   
   return(output)
 }
+
+printOut <- function(script){
+  nth<-paste0(script, c(rep(",", length(script))))
+  print(nth)
+}  
+  
+  
+ gr <-c( 
+   #values calculated from static_bootstrap_error
+   #need to use these to create a expected coefficient for each length of data      
+   1.59355109511403, 2.27434764081231, 1.80701533003434, 2.8033070905804,  2.47273118124658,
+   2.2678094766454,  1.70293648265862, 1.75824435066782, 2.39994853939748, 2.05618556035496,
+   1.73291957577073, 3.11727859002214, 2.10249651986484, 2.67586029410928, 2.76878761718571,
+   2.75785692702678, 4.87509641573421, 2.22512584062143, 2.03842524505992, 2.37116906755947,
+   1.5474823207671,  2.55967603965411, 2.25032612965191, 2.24384714649027, 2.67298196948062,
+   2.75206181348804, 2.3844308779272,  1.86951557520666, 2.90789763990639, 2.39609079059703,
+   2.36630467691164, 1.65735179688503, 1.92602291823894, 3.16666079285206, 2.55407626276911,
+   2.71336027282355, 2.70211719086013, 2.74081541080886, 2.10870397995379, 2.58190811840651,
+   2.91100111458016, 2.5324076524782,  2.01231573699737, 1.80634053283447, 2.73435942159519,
+   2.80245913502402, 2.64791378874629, 3.00301313866373, 2.69935421670575, 1.94230421909558,
+   2.16407636957608, 1.44827011693701, 2.72442517260475, 1.99176484428726, 2.49710108434088,
+   2.35418251227281, 2.57755205493521, 3.25433632142714, 2.14215706008829, 1.94472238634253,
+   2.88476115509466, 1.82735593220973, 2.2774540267339,  1.67420231173741, 3.44292470484641,
+   2.40759330752955, 2.68237696493158, 2.17008872899264, 2.79188401240115, 2.13069343600699,
+   2.66100024669349, 1.80807501956531, 2.50112856124061, 2.0369523705705,  2.21318887583437,
+   2.41189321549167, 2.99824201791627, 3.79203106205445, 1.67307440964618, 2.51447446833737,
+   1.77702995440098, 1.5505891998448,  2.58992961159756, 2.07853452213866, 1.7625054324503, 
+   2.62990767012543, 2.23781938670282, 2.62106475697716, 2.21957314044713, 3.04026112958293,
+   1.88810850395291, 2.1071713219079,  2.69014396012727, 2.30362612067092, 2.46792439170224,
+   2.67455884182336, 1.65455090250905, 3.87022184984458, 2.92549773370628, 2.16536099742458
+   )
--- simulation_study.R
+++ simulation_study.R
@@ -10,11 +10,14 @@
 # To generate the data, we will be using the models: blocks, fms, mix, teeth10, stairs10, and extreme teeth,
 # with a selection of parameters
 
+# thanks goes to () for their work in the implementation and the associated papers ()
+# as well as the 'mosum'
+
 library('changepoint')
 library('stepR')
 library('FDRSeg')
 library('wbs')
-
+library('mosum')
 
 #blocks model
 blocks <- function(variance = 1,changepoints, means, size){
@@ -38,83 +41,127 @@ determine <- function(test_value, true_value){
   return(as.numeric(logical))
 }
 
-store <- list(designation = "This is a list of the testing metrics for the simulation study")
-modelList <- list("blocks", "fms", "teeth10", "stairs10", "mix")
-
-MCsimulation <- function(li1, modelList, li3, N){
+handle <- function(foo){
+  output <- NULL
+  sample.mean <- NULL
+  sample.var <- NULL
+  a <- c(-1,0,1)
+  N <- length(foo[1,])
   
-  store <- NULL
-  modelList <- c("blocks", "fms", "teeth10", "stairs10", "mix")
+  for(i in 1:length(foo[,1])){
+    sample.mean <- c(sample.mean, mean(rep(a, times =  foo[i,])))
+    
+    sample.var <-c(sample.var, var(rep(a, times =  foo[i,])))
+  }
+  return(cbind(sample.mean, sample.var))
+}
+
+
+store <- list(designation = "This is a list of the testing metrics for the simulation study")
+modelList <- c("blocks", "fms", "teeth10", "stairs10", "mix")
+
+MCsimulation <- function(models = NULL, N){
+  
+  modelList <- models
+  
+  store <- list(designation = "This is a list of the testing metrics for the simulation study")
+  if(is.null(models)){modelList <- c("blocks", "fms", "teeth10", "stairs10", "mix")}
   zero.triple <- c(0, 0, 0)
   
-  for(i in 1:length(ModelList)){
+  
+  for(i in 1:length(modelList)){
     
     difference_in_changepoints <- rbind(zero.triple, zero.triple, zero.triple, zero.triple, zero.triple)
     difference_in_means <- c(0,0,0,0,0)
+    changepoint.count <- NULL
+    
+    obs <- mosum::testData(model = modelList[i])
+    qfs <- simulQuantile(alpha = 0.5, length(obs), type = "fdrseg")
+    dfs <- simulQuantile(alpha = 0.5, length(obs), type = "smuce") 
     
     for(j in 1:N){
-     
       
+      if((j %in% (ceiling(1:N/2)*2)) | j <= 10){
+        counter <- c(i,j)
+        print(counter)
+        
+      }
+     
       obs <- (mosum::testData(model = modelList[i]))
       
       sd <- sdrobnorm(obs)
       sigma <- mad(obs)
-      qfs <- simulQuantile(alpha = 0.5, length(obs), type = "fdrseg")
-      dfs <- simulQuantile(alpha = 0.5, length(obs), type = "smuce") 
       
-      # using alpha = 0.5 here not sure if that is appropiate
+      # using alpha = 0.5 here, as suggested by the authors of the package
       
       obs.wbs <- wbs(obs)
-      obs.PELT <- changepoint::cpt.mean(obs*(1/sigma), method = "PELT")
-      obs.FDRSeg <- fdrseg(obs, qfs, sd = sd)
+      
+      # the PELT method requires the data to variance to be standardised
+      obs.PELT <- changepoint::cpt.mean(obs*(1/sd), method = "PELT")
+      obs.FDRSeg <- fdrseg(obs, qfs, sd = sigma) 
       obs.myBinSeg <- Binary.Segmentation(obs)
       obs.SMUCE <- stepFit(obs, q = dfs)
       
  # The PELT and SMUCE methods use a different definition of changepoint to our other models
  # so we must change it
-        obs.PELT@cpts <- obs.PELT@cpts -rep(1, length(obs.PELT@cpts))
-        obs.SMUCE$rightEnd <- obs.SMUCE$rightEnd - rep(1, length(obs.SMUCE$rightEnd))
+        #obs.PELT@cpts <- obs.PELT@cpts - 1
+        obs.SMUCE$leftEnd <- obs.SMUCE$leftEnd - 1
  # We want to compare the model's estimations of the number of changepoints
  # against the true number of changepoints.
         
-      true_changepoints <- which(abs(diff(mosum::testSignal(model = modelList[i])$mu_t)=!0))
+      true_changepoints <- which(abs(diff(mosum::testSignal(model = modelList[i])$mu_t))!=0)
       no.true_changepoints <- length(true_changepoints)
       
       A <- determine(obs.wbs$cpt$no.cpt.th, no.true_changepoints)
       B <- determine(length(obs.PELT@cpts), no.true_changepoints)
       C <- determine(length(obs.FDRSeg$value), no.true_changepoints)
       D <- determine(length(obs.myBinSeg$est.means), no.true_changepoints)
-      E <- determine(length(obs.SMUCE$rightEnd), no.true_changepoints)
+      E <- determine(length(obs.SMUCE$leftEnd), no.true_changepoints)
       
       difference_in_changepoints <- unname(difference_in_changepoints) + rbind(A, B, C, D, E)
+      changepoint.count <- rbind(changepoint.count, cbind(obs.wbs$cpt$no.cpt.th, length(obs.PELT@cpts), length(obs.FDRSeg$value), length(obs.myBinSeg$est.means), length(obs.SMUCE$leftEnd)))
       
  # We also want to compare the estimated model against the true model.
+ # some of the results require preprocessing
+      
+      obs.FDRSeg$left[1] <- 0
+      obs.FDRSeg$left <- c(obs.FDRSeg$left, obs.FDRSeg$n)
       
       true_signal <- mosum::testSignal(model = modelList[i])$mu_t
       
       A <- sum(abs(means.between.cpt(obs, cpt = obs.wbs$cpt$cpt.th[[1]]) - true_signal))/length(obs)
-      B <- sum(abs(rep(obs.PELT@param.est$mean, obs.PELT@cpts) - true_signal))/length(obs)
-      C <- sum(abs(rep(obs.FDRSeg$value, diff(c(obs.FDRSeg$left, obs.FDRSeg$n)) - true_signal)))/length(obs)
+      B <- sum(abs(rep(obs.PELT@param.est$mean, diff(c(0,obs.PELT@cpts))) - true_signal))/length(obs)
+      
+      h <- rep(obs.FDRSeg$value, times = diff(obs.FDRSeg$left))
+      j <- abs(h - true_signal)
+      C <- sum(j)/length(obs)
+      
+    
       D <- sum(abs(obs.myBinSeg$est.signal - true_signal))/length(obs)
-      E <- sum(abs(rep(obs.SMUCE$value, diff(c(obs.SMUCE$leftEnd, length(true_signal)))) - true_signal))/length(obs)
+      E <- sum(abs(rep(obs.SMUCE$value, diff(c(0, obs.SMUCE$rightEnd))) - true_signal))/length(obs)
+    
       
       difference_in_means <- difference_in_means + rbind(A, B, C, D, E)
       
+      
     }
     
-    foo <- difference_in_changepoints
-    a <- c(-1,0,1)
-    const <- 1/N
-    mean_difference <- rbind(const*sum(foo[1,]* a), const*sum(foo[2,]* a), const*sum(foo[3,]* a), const*sum(foo[4,]* a))
-    var_difference <- rbind(const*sum((foo[1,]*a)^2), const*sum((foo[2,]*a)^2), const*sum((foo[3,]*a)^2), const*sum((foo[4,]*a)^2))
+    foo <- changepoint.count
+    #print(foo)
+
+    mean_difference <- rbind(mean(foo[,1]), mean(foo[,2]), mean(foo[,3]), mean(foo[,4]), mean(foo[,5]))
+    var_difference <- rbind(var(foo[,1]), var(foo[,2]), var(foo[,3]), var(foo[,4]), var(foo[,5]))
     
-    bar <- difference_in_means
-    Mu_difference <- bar * rep(const, length(bar))
     
-    interim <- cbind(difference_in_changepoints, mean_difference, var_difference, Mu_difference)
+    pass_in <- cbind(mean_difference, var_difference)
     
-    store <- c(store, interim)
+    MAE <- difference_in_means / N
+    
+    interim <- cbind(difference_in_changepoints, pass_in, MAE)
+
+    store <- c(store, list(interim))
+    print(interim)
   }
-  names(store) <- modelList
+  names(store) <- c("designation", modelList)
   return(store)
 }
